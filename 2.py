61a1331c8a1bfb5a52846ebf1e8238e0bb301b3d
Dheeraj Reddy Pailla <dheerajpreddy@gmail.com>
2018-03-13 02:53:54 +0530

    Done with v1, need to remove print statements and clean further

--- 2.py
+++ 2.py
@@ -1,49 +1,73 @@
 import sys, math, copy
 
 
-class ValueIterator:
+class MDP:
 
-    def __init__(self, world, policy, prev_world, read_only, delta, prob_target=1.0, prob_left=0.0, prob_right=0.0, \
-                prob_back=0.0, state_reward=0.0, discount_factor=1.0 ):
-        self.world = world
+    def __init__(self, board, policy, walls, end_states, step_reward, start):
+        self.board = board
+        self.walls = walls
+        self.start = start
+        self.old_board = copy.deepcopy(self.board)
+        self.end_states = end_states
         self.policy =policy
-        self.prev_world = prev_world
-        self.prob_target = prob_target
-        self.prob_left = prob_left
-        self.prob_right = prob_right
-        self.prob_back = prob_back
-        self.state_reward = state_reward
-        self.read_only_states = read_only
-        self.discount_factor = discount_factor
-        self.delta = delta
+        self.probability = {
+        'target' : 0.8,
+        'alt':     0.1,
+        }
+        self.delta = 0.85
+        self.step_reward = step_reward
+        self.noop_states = copy.deepcopy(self.end_states)
+        self.init_board()
+        self.init_policy()
+        self.value_iteration()
 
-    def iterate(self, iterations):
-        for i in range(iterations):
+    def init_board(self):
+        """Initializing the board with the walls. Replacing walls with NaN."""
+        for i in range(len(self.walls)):
+            x, y = self.walls[i]
+            self.board[x][y] =  None
+            self.policy[x][y] = None
+            self.noop_states.append(walls[i])
+
+    def init_policy(self):
+        """Initializing policy for the board."""
+        for i in range(len(self.end_states)):
+            x, y = self.end_states[i]
+            print self.board[x][y]
+            if self.board[x][y] > 0:
+                self.policy[x][y] = "Goal"
+            else:
+                self.policy[x][y] = "Bad"
+
+    def value_iteration(self):
+        i = 0
+        while True:
+            i += 1
             print ('\n')
             print ('\n')
             print('_' * 80)
 
 
-            for row in range(len(self.world)):
-                for col in range(len(self.world[row])):
-                    if (row, col) in self.read_only_states:
+            for row in range(len(self.board)):
+                for col in range(len(self.board[row])):
+                    if (row, col) in self.noop_states:
                         continue
 
                     print('Iteration %d' % int(i + 1))
                     print('Estimating S(%d,%d)' % (row, col))
-                    self.world[row][col] = self.bellman_update((row, col))
+                    self.board[row][col] = self.bellman_update((row, col))
 
-                    self.print_world(decimal_places=4)
+                    self.print_board(decimal_places=4)
 
-            #Before updating the previous world to reflect the new world changes, we need to check for diff and if diff < delta
+            #Before updating the previous board to reflect the new board changes, we need to check for diff and if diff < delta
             #How do we do that? Trying to implement...
             list = []
-            for row in range(len(self.world)):
-                for col in range(len(self.world[row])):
-                    if (row, col) in self.read_only_states:
+            for row in range(len(self.board)):
+                for col in range(len(self.board[row])):
+                    if (row, col) in self.noop_states:
                         continue
 
-                    list.append((self.world[row][col] - self.prev_world[row][col]))
+                    list.append((self.board[row][col] - self.old_board[row][col]))
 
             print "List for utility change: "
             print list
@@ -70,21 +94,21 @@ class ValueIterator:
                 return
 
 
-            #Update the previous world to the new world
-            #print self.prev_world
-            #print self.world
+            #Update the previous board to the new board
+            #print self.old_board
+            #print self.board
 
-            for row in range(len(self.world)):
-                for col in range(len(self.world[row])):
-                    if (row, col) in self.read_only_states:
+            for row in range(len(self.board)):
+                for col in range(len(self.board[row])):
+                    if (row, col) in self.noop_states:
                         continue
-                    self.prev_world[row][col] = self.world[row][col]
+                    self.old_board[row][col] = self.board[row][col]
 
     def policy_formation(self):
 
-        for row in range(len(self.world)):
-            for col in range(len(self.world[row])):
-                if (row, col) in self.read_only_states:
+        for row in range(len(self.board)):
+            for col in range(len(self.board[row])):
+                if (row, col) in self.noop_states:
                     continue
                 print('Estimating S(%d,%d)' % (row, col))
                 self.policy[row][col] = self.policy_update((row, col))
@@ -103,10 +127,10 @@ class ValueIterator:
         w_coords = (r, c - 1)
         n_coords = (r - 1, c)
 
-        e_util = self.get_utility_of_policy(self.world[r][c], e_coords)
-        s_util = self.get_utility_of_policy(self.world[r][c], s_coords)
-        w_util = self.get_utility_of_policy(self.world[r][c], w_coords)
-        n_util = self.get_utility_of_policy(self.world[r][c], n_coords)
+        e_util = self.get_utility_of_policy(self.board[r][c], e_coords)
+        s_util = self.get_utility_of_policy(self.board[r][c], s_coords)
+        w_util = self.get_utility_of_policy(self.board[r][c], w_coords)
+        n_util = self.get_utility_of_policy(self.board[r][c], n_coords)
 
 
         #print('e_util%s = %f' % (e_coords, e_util))
@@ -116,21 +140,21 @@ class ValueIterator:
 
         # print 'E: '
         e_value = self.value_function(e_util, n_util, s_util, w_util)
-        print('e_value%s = %f' % (e_coords, self.state_reward+ e_value))
+        print('e_value%s = %f' % (e_coords, self.step_reward+ e_value))
         print  'S: '
         s_value = self.value_function(s_util, e_util, w_util, n_util)
-        print('s_value%s = %f' % (s_coords, self.state_reward+s_value))
+        print('s_value%s = %f' % (s_coords, self.step_reward+s_value))
         print 'W: '
         w_value = self.value_function(w_util, s_util, n_util, e_util)
-        print('w_value%s = %f' % (w_coords, self.state_reward+w_value))
+        print('w_value%s = %f' % (w_coords, self.step_reward+w_value))
         print 'N: '
         n_value = self.value_function(n_util, w_util, e_util, s_util)
-        print('n_value%s = %f' % (n_coords, self.state_reward+n_value))
+        print('n_value%s = %f' % (n_coords, self.step_reward+n_value))
 
         print ('\n')
-        print 'MAX IS : ' + str(max(self.state_reward+e_value, self.state_reward+s_value, self.state_reward+w_value, self.state_reward+n_value))
-        xd =  (self.state_reward) + self.discount_factor * max(e_value, s_value, w_value, n_value)
-        print 'Total utility for this state: ' #+ (self.state_reward) + ' ' + '+' +' '+ (self.discount_factor)+ ' '+ '*'+ ' '+ 'max( '+e_value+' '+s_val
+        print 'MAX IS : ' + str(max(self.step_reward+e_value, self.step_reward+s_value, self.step_reward+w_value, self.step_reward+n_value))
+        xd =  (self.step_reward) + max(e_value, s_value, w_value, n_value)
+        print 'Total utility for this state: ' #+ (self.step_reward) + ' ' + '+' +' '+ (self.discount_factor)+ ' '+ '*'+ ' '+ 'max( '+e_value+' '+s_val
         print xd
 
         #Finally, return the appropriate letter to be filled
@@ -152,12 +176,12 @@ class ValueIterator:
         w_coords = (r, c - 1)
         n_coords = (r - 1, c)
 
-        e_util = self.get_utility_of_state(self.prev_world[r][c], e_coords)
-        s_util = self.get_utility_of_state(self.prev_world[r][c], s_coords)
-        w_util = self.get_utility_of_state(self.prev_world[r][c], w_coords)
-        n_util = self.get_utility_of_state(self.prev_world[r][c], n_coords)
+        e_util = self.get_utility_of_state(self.old_board[r][c], e_coords)
+        s_util = self.get_utility_of_state(self.old_board[r][c], s_coords)
+        w_util = self.get_utility_of_state(self.old_board[r][c], w_coords)
+        n_util = self.get_utility_of_state(self.old_board[r][c], n_coords)
 
-        print('own_value%s = %d' % ((r, c), self.world[r][c]))
+        print('own_value%s = %d' % ((r, c), self.board[r][c]))
         #print('e_util%s = %f' % (e_coords, e_util))
         #print('s_util%s = %f' % (s_coords, s_util))
         #print('w_util%s = %f' % (w_coords, w_util))
@@ -165,25 +189,25 @@ class ValueIterator:
 
         print 'E: '
         e_value = self.value_function(e_util, n_util, s_util, w_util)
-        print('e_value%s = %f' % (e_coords, self.state_reward+ e_value))
+        print('e_value%s = %f' % (e_coords, self.step_reward+ e_value))
         print  'S: '
         s_value = self.value_function(s_util, e_util, w_util, n_util)
-        print('s_value%s = %f' % (s_coords, self.state_reward+s_value))
+        print('s_value%s = %f' % (s_coords, self.step_reward+s_value))
         print 'W: '
         w_value = self.value_function(w_util, s_util, n_util, e_util)
-        print('w_value%s = %f' % (w_coords, self.state_reward+w_value))
+        print('w_value%s = %f' % (w_coords, self.step_reward+w_value))
         print 'N: '
         n_value = self.value_function(n_util, w_util, e_util, s_util)
-        print('n_value%s = %f' % (n_coords, self.state_reward+n_value))
+        print('n_value%s = %f' % (n_coords, self.step_reward+n_value))
 
         print ('\n')
-        print 'MAX IS : ' + str(max(self.state_reward+e_value, self.state_reward+s_value, self.state_reward+w_value, self.state_reward+n_value))
+        print 'MAX IS : ' + str(max(self.step_reward+e_value, self.step_reward+s_value, self.step_reward+w_value, self.step_reward+n_value))
 
 
 
 
-        xx =  (self.state_reward) + self.discount_factor * max(e_value, s_value, w_value, n_value)
-        print 'Total utility for this state: ' #+ (self.state_reward) + ' ' + '+' +' '+ (self.discount_factor)+ ' '+ '*'+ ' '+ 'max( '+e_value+' '+s_val
+        xx =  (self.step_reward) +  max(e_value, s_value, w_value, n_value)
+        print 'Total utility for this state: ' #+ (self.step_reward) + ' ' + '+' +' '+ (self.discount_factor)+ ' '+ '*'+ ' '+ 'max( '+e_value+' '+s_val
         print xx
         return xx
     def get_utility_of_state(self, own_value, target_coords):
@@ -194,7 +218,7 @@ class ValueIterator:
             return own_value
 
         try:
-            value = self.prev_world[row][col] or own_value
+            value = self.old_board[row][col] or own_value
         except IndexError:
             print('Bumping against a wall in S(%d,%d)' % (row, col))
             value = own_value
@@ -209,7 +233,7 @@ class ValueIterator:
             return own_value
 
         try:
-            value = self.world[row][col] or own_value
+            value = self.board[row][col] or own_value
         except IndexError:
             print('Bumping against a wall in S(%d,%d)' % (row, col))
             value = own_value
@@ -217,20 +241,20 @@ class ValueIterator:
 
 
     def value_function(self, target, left=0, right=0, back=0):
-        xy = float(self.prob_target * target + \
-            self.prob_left * left + \
-            self.prob_right * right +\
-            self.prob_back * back)
+        xy = float(self.probability['target'] * target + \
+            self.probability['alt'] * left + \
+            self.probability['alt'] * right +\
+            self.probability['alt'] * back)
 
-        print "-0.85 + " +str(self.prob_target) + str("*(")+ str(target)+str(") + ") +str(self.prob_left) + str("*(")+str(left)+str(") + ")+str(self.prob_right) + str("*(")+str(right)+str(") ")
+        print "-0.85 + " +str(self.probability['target']) + str("*(")+ str(target)+str(") + ") +str(self.probability['alt']) + str("*(")+str(left)+str(") + ")+str(self.probability['alt']) + str("*(")+str(right)+str(") ")
         return xy
 
-    def print_world(self, decimal_places=0):
-        for row in range(len(self.world)):
+    def print_board(self, decimal_places=0):
+        for row in range(len(self.board)):
             print('-' * 70)
             sys.stdout.write(str(row))
-            for col in range(len(self.world[row])):
-                val = self.world[row][col]
+            for col in range(len(self.board[row])):
+                val = self.board[row][col]
                 if type(val) == float:
                     val = round(val, decimal_places)
                 sys.stdout.write(' | %14s' % val)
@@ -248,32 +272,6 @@ class ValueIterator:
 
 
 if __name__ == '__main__':
-
-    # world = [
-    #             [None, None, 17, None],
-    #             [0,   0,  0,   0],
-    #             [0, -17, None, 0],
-    #             [0, 0, 0, 0]
-    #         ]
-    # prev_world = [
-    #             [None, None, 17, None],
-    #             [0,   0,  0,   0],
-    #             [0, -17, None, 0],
-    #             [0, 0, 0, 0]
-    #         ]
-    #
-    # policy = [
-    #             [None, None, "Goal", None],
-    #             ["n/a", "n/a", "n/a", "n/a"],
-    #             ["n/a", "Pit", None, "n/a"],
-    #             ["n/a" , "n/a", "n/a", "n/a"]
-    # ]
-    # read_only_states = [(0, 0), (0, 1), (0, 2), (0,3), (2,1), (2,2)]
-    #
-    # # For stochastic actions
-    # Vi_stochastic = ValueIterator(world, policy, prev_world,read_only_states, prob_target=0.8, prob_left=0.1, prob_right=0.1, state_reward=-0.85 , delta=0.85)
-    #
-    # Vi_stochastic.iterate(100)
 
     # Taking input for size of board
     inp = raw_input()
@@ -324,21 +322,6 @@ if __name__ == '__main__':
 
     # Taking input for unit step reward
     unit_step_reward = float(raw_input())
-    read_only_states = copy.deepcopy(end_states)
-    for i in range(len(walls)):
-        x, y = walls[i]
-        board[x][y] =  None
-        policy[x][y] = None
-        read_only_states.append(walls[i])
-    for i in range(len(end_states)):
-        x, y = end_states[i]
-        print board[x][y]
-        if board[x][y] > 0:
-            policy[x][y] = "Goal"
-        else:
-            policy[x][y] = "Bad"
-    old_board = copy.deepcopy(board)
-    # m = MDP(board, end_states, walls, start, unit_step_reward, policy)
-    Vi_stochastic = ValueIterator(board, policy, old_board,read_only_states, prob_target=0.8, prob_left=0.1, prob_right=0.1, state_reward=unit_step_reward , delta=0.85)
 
-    Vi_stochastic.iterate(100)
+    # Creating class object and beginning value iteration
+    m = MDP(board, policy, walls, end_states, unit_step_reward, start)
